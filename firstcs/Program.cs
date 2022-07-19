53e433ba4642c0b43a186ae1c27dcafc7b6753f7
Jonas Chuan <jc.chuan.0303@gmail.com>
2022-07-19 17:04:10 +0800

    Add project files.

---
+++ firstcs/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace HelloWorld {
+    class Program {
+        static void xMain(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
