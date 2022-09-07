f6cfc3ea9d6babe0e1132807d4d29fa2365411ed
PoisonDarterz <jc.chuan.0303@gmail.com>
2022-09-07 19:09:18 +0800

    array pracs

---
+++ firstcs/exercise/w3array.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+
+namespace Exercise {
+    class w3arr {
+        static void Main(string[] args) {
+            int[] array1 = {1, 2, 2, 3, 3, 4, 5, 6, 5, 7, 7, 7, 8, 8, 1};
+            int sum = 0;
+            //q47
+            for (int i=0; i<array1.Length; i++) {
+                sum += array1[i];
+            }
+            Console.WriteLine("The sum is " + sum);
+            Console.WriteLine();
+
+            //q48
+            if ((array1.First() == array1.Last()) && (array1.Length >= 1)) {
+                Console.WriteLine("True");
+            } else {
+                Console.WriteLine("False");
+            }
+            Console.WriteLine();
+
+            //q50
+            Queue<int> q50arr = new Queue<int> (new int[] { 1, 2, 8 });
+            int q50a = q50arr.Dequeue();
+            q50arr.Enqueue(q50a);
+            foreach (int i in q50arr) {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+
+            //q51
+            int[] q51arr = { 1, 2, 5, 7, 8 };
+            int q51h;
+            if (q51arr.First() > q51arr.Last()) {
+                q51h = q51arr.First();
+            } else {
+                q51h = q51arr.Last();
+            }
+            Console.WriteLine("The higher value is: " + q51h);
+            Console.WriteLine();
+
+            //q52
+            int[] q52arr1 = { 1, 2, 5 };
+            int[] q52arr2 = { 0, 3, 8 };
+            int[] q52arr3 = { -1, 0, 2 };
+            int[] q52arr = { q52arr1[1], q52arr2[1], q52arr3[1] };
+            Console.WriteLine("New array: ");
+            foreach (int i in q52arr) {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine("\n");
+
+            //q57
+            List<int> q57arr = new List<int>();
+            Console.WriteLine("Input size of integer: ");
+            int q57s = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < q57s; i++) {
+                Console.Write("Input a number: ");
+                q57arr.Add(Convert.ToInt32(Console.ReadLine()));
+            }
+            int q57large = -9999999;
+            int q57prod;
+            for (int j = 0; j < (q57s-1); j++) {
+                q57prod = q57arr[j] * q57arr[j + 1];
+                if (q57prod > q57large) {
+                    q57large = q57prod;
+                }
+            }
+            Console.WriteLine("Array: ");
+            foreach (int i in q57arr) {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine("\nThe largest product between adjacent elements: " + q57large);
+        }
+    }
+}
--- firstcs/exercise/w3exercise2.cs
+++ firstcs/exercise/w3exercise2.cs
@@ -2,59 +2,59 @@
 
 namespace Exercise {
     class w3e2 {
-        static void Main(string[] args) {
-            ////q27
-            //Console.WriteLine("Enter a number: ");
-            //int q27a = Convert.ToInt32(Console.ReadLine());
-            //int sum = 0;
-            //while (q27a != 0) {
-            //    sum += (q27a % 10);
-            //    q27a /= 10;
-            //}
-            //Console.WriteLine("The sum of the digits is " + sum);
-            //Console.WriteLine();
+        static void xMain(string[] args) {
+            //q27
+            Console.WriteLine("Enter a number: ");
+            int q27a = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            while (q27a != 0) {
+                sum += (q27a % 10);
+                q27a /= 10;
+            }
+            Console.WriteLine("The sum of the digits is " + sum);
+            Console.WriteLine();
 
-            ////q31
-            //int[] q31a = new int[4];
-            //int[] q31b = new int[4];
-            //int[] q31c = new int[4];
-            //for (int i = 0; i < 4; i++) {
-            //    Console.WriteLine("Enter a number: ");
-            //    q31a[i] = Convert.ToInt32(Console.ReadLine());
-            //}
-            //for (int j = 0; j < 4; j++) {
-            //    Console.WriteLine("Enter a number: ");
-            //    q31b[j] = Convert.ToInt32(Console.ReadLine());
-            //}
-            //for (int k = 0; k < 4; k++) {
-            //    q31c[k] = q31a[k] * q31b[k];
-            //}
-            //Console.WriteLine("Array 1: ");
-            //for (int l = 0; l < 4; l++) {
-            //    Console.Write(q31a[l] + " ");
-            //}
-            //Console.WriteLine();
-            //Console.WriteLine("Array 2: ");
-            //for (int m = 0; m < 4; m++) {
-            //    Console.Write(q31b[m] + " ");
-            //}
-            //Console.WriteLine();
-            //Console.WriteLine("Multiplied: ");
-            //for (int n = 0; n < 4; n++) {
-            //    Console.Write(q31c[n] + " ");
-            //}
-            //Console.WriteLine("\n");
+            //q31
+            int[] q31a = new int[4];
+            int[] q31b = new int[4];
+            int[] q31c = new int[4];
+            for (int i = 0; i < 4; i++) {
+                Console.WriteLine("Enter a number: ");
+                q31a[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (int j = 0; j < 4; j++) {
+                Console.WriteLine("Enter a number: ");
+                q31b[j] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (int k = 0; k < 4; k++) {
+                q31c[k] = q31a[k] * q31b[k];
+            }
+            Console.WriteLine("Array 1: ");
+            for (int l = 0; l < 4; l++) {
+                Console.Write(q31a[l] + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Array 2: ");
+            for (int m = 0; m < 4; m++) {
+                Console.Write(q31b[m] + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Multiplied: ");
+            for (int n = 0; n < 4; n++) {
+                Console.Write(q31c[n] + " ");
+            }
+            Console.WriteLine("\n");
 
-            ////q36
-            //Console.WriteLine("Enter first number: ");
-            //int q36a = Convert.ToInt32(Console.ReadLine());
-            //Console.WriteLine("Enter second number: ");
-            //int q36b = Convert.ToInt32(Console.ReadLine());
-            //bool isrange = false;
-            //if ((q36a >= -10 && q36a <= 10) && (q36b >= -10 && q36b <= 10)) {
-            //    isrange = true;
-            //}
-            //Console.WriteLine(isrange);
+            //q36
+            Console.WriteLine("Enter first number: ");
+            int q36a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number: ");
+            int q36b = Convert.ToInt32(Console.ReadLine());
+            bool isrange = false;
+            if ((q36a >= -10 && q36a <= 10) && (q36b >= -10 && q36b <= 10)) {
+                isrange = true;
+            }
+            Console.WriteLine(isrange);
 
             //q39
             Console.WriteLine("Enter first number: ");
